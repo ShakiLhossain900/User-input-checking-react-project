52c6af44ff0bb427da7e82156f2321f92e0c7313
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-04-23 08:34:05 +0600

    adding something new

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,23 +1,24 @@
-import React from 'react';
-import Card from '../UI/Card';
-import classes from './AddUser.module.css'
+import React from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
-const AddUser = (props) =>{
-    const addUserHandler = (event)=>{
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
      event.preventDefault();
-    };
+  };
 
-    return (
-      <Card className={classes.input}>
+  return (
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
-          <label htmlFor="username">Username</label>
-          <input id="username" type="text" />
-          <label htmlFor="age">Age(Years)</label>
-          <input id="age" type="number" />
-          <button type="submit">Add User</button>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age(Years)</label>
+        <input id="age" type="number" />
+        <Button type="submit">Add User</Button>
       </form>
-      </Card>
-    );
+    </Card>
+  );
 };
 
 export default AddUser;
