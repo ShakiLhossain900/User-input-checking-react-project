3ae6a4565cc600e9fc03f4fced49ac7a502c738f
Gazi md Shakil <86937968+ShakiLhossain900@users.noreply.github.com>
2022-04-24 11:13:53 +0600

    comment something

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -7,6 +7,7 @@ import classes from "./AddUser.module.css";
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredUserAge, setEnteredUserAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
@@ -14,9 +15,17 @@ const AddUser = (props) => {
       enteredUsername.trim().length === 0 ||
       enteredUserAge.trim().length === 0
     ) {
+      setError({
+        title: "Invalid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredUserAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0).",
+      });
       return;
     }
     // console.log(enteredUsername, enteredUserAge);
@@ -32,9 +41,19 @@ const AddUser = (props) => {
     setEnteredUserAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something want wrong"  />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">Username</label>
